dd55de07b0dbd751cb10fa19b2ab14f87b13ceee
ansonsarturs <65274330+arturs-ansons@users.noreply.github.com>
2024-04-03 13:24:24 +0300

    Update router.js

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 // router.js
 import { createRouter, createWebHistory } from 'vue-router';
-import store from './store'; // Import your Vuex store
+import store from './store';
 import Register from './components/RegisterUser.vue';
 import Login from './components/LoginUser.vue';
 import Articles from './components/ArticleList.vue';
@@ -11,14 +11,13 @@ const routes = [
   { path: '/articles', component: Articles, meta: { requiresAuth: true } },
   { path: '/logout', meta: { requiresAuth: true }, 
     beforeEnter: (to, from, next) => {
-      // Perform logout action here
       store.dispatch('logout')
         .then(() => {
-          next('/login'); // Redirect to login after logout
+          next('/login');
         })
         .catch(error => {
           console.error('Logout error:', error);
-          next('/login'); // Redirect to login even if logout fails
+          next('/login');
         });
     }
   },
@@ -32,11 +31,11 @@ const router = createRouter({
 
 router.beforeEach((to, from, next) => {
   if (to.meta.requiresAuth && !store.getters.isAuthenticated) {
-    next('/login'); // Redirect to login if requiresAuth and not authenticated
+    next('/login');
   } else if (to.meta.requiresGuest && store.getters.isAuthenticated) {
-    next('/articles'); // Redirect to articles if requiresGuest and authenticated
+    next('/articles');
   } else {
-    next(); // Proceed with navigation
+    next();
   }
 });
 
